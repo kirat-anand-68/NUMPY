7fa094bb8ba96c9a263309dcef75b7761e77932d
Kirat Anand <167013352+kirat-anand-68@users.noreply.github.com>
2024-12-12 22:39:01 +0530

    Create Matplotlib.py

---
+++ Matplotlib.py
@@ -0,0 +1,12 @@
+#matplotlib-> seaborn is a library that uses the matplotlib underneath to plot the graph->
+#Displot is distribution plot(curve plot same as histogram)
+
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# sns.distplot([0,1,2,3,4,5])
+# plt.show()
+#Plotting a distplot withou histogram
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.distplot([0,1,2,3,4,5],hist=False)
+plt.show()
